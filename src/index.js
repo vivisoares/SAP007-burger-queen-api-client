318f696cb089fc2e427496dede76a6bc490ebaf7
Viviane Soares <vianesoaresss@gmail.com>
2022-05-20 01:40:27 -0300

    Need to fix CSS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import AllRoute from './routes/route';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AllRoute/>
   </React.StrictMode>
 );
