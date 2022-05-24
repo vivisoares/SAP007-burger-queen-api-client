72d9bb5cd6bebca99cd4d8757de67f7dffef6e17
Viviane Soares <vianesoaresss@gmail.com>
2022-05-23 23:12:14 -0300

    AGORA

--- src/pages/order/order.jsx
+++ src/pages/order/order.jsx
@@ -23,7 +23,7 @@ const Order = () => {
                 <MenuHamburguer />
               </div>
               <picture>
-                <img src={logo} alt="Logotipo Vai Dar Bom" className={styles.logo} />
+                <img src={logo} alt="no ponto certo" className={styles.logo} />
               </picture>
             </section>
           </nav>
